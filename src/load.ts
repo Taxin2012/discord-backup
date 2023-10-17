15e4d8be39e68aca4cad1cb217657d464142d4db
Taxin2012 <26491089+Taxin2012@users.noreply.github.com>
2023-10-17 07:01:15 +0300

    Update load.ts

--- src/load.ts
+++ src/load.ts
@@ -86,13 +86,9 @@ export const loadChannels = (guild: Guild, backupData: BackupData, options: Load
             })
         );
     });
-    backupData.channels.others.forEach((channelData) => {
-        console.log('RESTORE-OTHER ', channelData.name);
-        
-        //if (channelData.name != 'rules' && channelData.name != 'moderator-only') {
-            loadChannelPromises.push(loadChannel(channelData, guild, null, options));
-        //}
-    });
+    /*backupData.channels.others.forEach((channelData) => {
+        loadChannelPromises.push(loadChannel(channelData, guild, null, options));
+    });*/
     return Promise.all(loadChannelPromises);
 };
 
