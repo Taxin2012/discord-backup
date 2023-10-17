2b3fb2e7e1cde562329ac49cbedd4eb54bad3b93
Taxin2012 <26491089+Taxin2012@users.noreply.github.com>
2023-10-17 06:58:04 +0300

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -218,7 +218,7 @@ export async function loadChannel(
         const loadMessages = (channel: TextChannel | ThreadChannel, messages: MessageData[], previousWebhook?: Webhook): Promise<Webhook|void> => {
             return new Promise(async (resolve) => {
                 let channelName = (channel as TextChannel).name;
-                if (!category) console.log('RESTORING NOT CAT', channelName);
+                if (!category) return resolve();
                 
                 const webhook = previousWebhook || await (channel as TextChannel).createWebhook({
                     name: 'MessagesBackup',
