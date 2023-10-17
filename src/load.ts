5c6b5ad845c8ae77622bfd4207a6a8dacdb68fec
Taxin2012 <26491089+Taxin2012@users.noreply.github.com>
2023-10-17 07:02:58 +0300

    Update load.ts

--- src/load.ts
+++ src/load.ts
@@ -79,7 +79,7 @@ export const loadChannels = (guild: Guild, backupData: BackupData, options: Load
             new Promise((resolve) => {
                 loadCategory(categoryData, guild).then((createdCategory) => {
                     categoryData.children.forEach((channelData) => {
-                        loadChannel(channelData, guild, createdCategory, options);
+                        //loadChannel(channelData, guild, createdCategory, options);
                         resolve(true);
                     });
                 });
